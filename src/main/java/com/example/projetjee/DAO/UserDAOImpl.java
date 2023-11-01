d87a823fe044ddc40a4cb2951627d455dcf2532b
MaximeBACQ <maxhd95@gmail.com>
2023-11-01 14:19:51 +0100

    mini up

--- src/main/java/com/example/projetjee/DAO/UserDAOImpl.java
+++ src/main/java/com/example/projetjee/DAO/UserDAOImpl.java
@@ -1,4 +1,44 @@
 package com.example.projetjee.DAO;
 
-public class UserDAOImpl {
+import com.example.projetjee.Hibernate.HibernateUtil;
+import com.example.projetjee.Hibernate.SiteUser;
+import com.example.projetjee.Hibernate.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+
+import java.util.List;
+
+
+public class UserDAOImpl implements UserDAO{
+    @Override
+    public void addUser(SiteUser user) {
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+
+            //security
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public SiteUser getUserById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<SiteUser> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void updateUser(SiteUser userInfos) {
+
+    }
+
+    @Override
+    public void deleteUser(int id) {
+
+    }
 }
--- src/main/java/com/example/projetjee/Hibernate/HibernateUtil.java
+++ src/main/java/com/example/projetjee/Hibernate/HibernateUtil.java
@@ -1,6 +1,7 @@
 package com.example.projetjee.Hibernate;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -11,18 +12,21 @@ public class HibernateUtil{
     private static  SessionFactory sessionFactory;
 
     private static SessionFactory buildSessionFactory() {
+        File configFile = new File("hibernate.cfg.xml");
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure(configFile)
+                .build();
         try {
-            File configFile = new File("hibernate.cfg.xml");
-            StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                    .configure(configFile)
-                    .build();
-
-            return new MetadataSources(registry)
+            sessionFactory = new MetadataSources(registry)
+                    .addAnnotatedClass(SiteUser.class)
                     .buildMetadata()
                     .buildSessionFactory();
+            return sessionFactory;
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new ExceptionInInitializerError("Initial SessionFactory creation failed.");
+            System.err.println("Initialisation de la SessionFactory a échoué : " + e);
+            StandardServiceRegistryBuilder.destroy(registry);
+
+            throw new ExceptionInInitializerError(e);
         }
     }
 
