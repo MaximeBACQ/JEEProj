5db25ac340f1278d765d2ef47764ae3ea9a60a1f
Lucaslas28 <lucasrigot@gmail.com>
2023-11-08 00:19:04 +0100

    mail fonctionnel

--- src/main/java/com/example/projetjee/MailSender.java
+++ src/main/java/com/example/projetjee/MailSender.java
@@ -8,7 +8,7 @@ public class MailSender {
         // Configuration des propriétés du serveur SMTP
         Properties properties = new Properties();
         properties.put("mail.smtp.host", "smtp.gmail.com"); // Remplacez par le serveur SMTP de votre fournisseur de messagerie
-        properties.put("mail.smtp.port", "465"); // Port SMTP
+        properties.put("mail.smtp.port", "587"); // Port SMTP
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.enable", "true");
 
@@ -16,14 +16,14 @@ public class MailSender {
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("javaee20112023@gmail.com", "MaximeNinoMathisLucas");
+                return new PasswordAuthentication("javaee20112023@gmail.com", "eryo diho sryt smtz");
             }
         });
 
         try {
             // Création de l'objet Message
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("javaee20112023@gmail.com"));
+            //message.setFrom(new InternetAddress("javaee20112023@gmail.com"));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
             message.setSubject(subject);
             message.setText(body);
