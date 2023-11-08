b50b1c09258bada9dd13311c5936e67a7c1012ce
Nino-Hamel <Nino.Hamel@yahoo.fr>
2023-11-08 02:27:21 +0100

    database schema updated for relationships

--- database.sql
+++ database.sql
@@ -1,44 +1,46 @@
 CREATE DATABASE IF NOT EXISTS database_JEE;
 
 USE database_JEE;
-DROP TABLE users;
 CREATE TABLE IF NOT EXISTS users (
-    userId INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    surname VARCHAR(50),
-    username VARCHAR(50),
-    email VARCHAR(100),
-    birthdate DATE,
-    gender VARCHAR(10),
-    password VARCHAR(100),
-    isAdmin BIT,
-    isModerator BIT
+                                     userId INT AUTO_INCREMENT PRIMARY KEY,
+                                     name VARCHAR(50),
+                                     surname VARCHAR(50),
+                                     username VARCHAR(50),
+                                     email VARCHAR(100),
+                                     birthdate DATE,
+                                     gender VARCHAR(10),
+                                     password VARCHAR(100),
+                                     isAdmin BIT,
+                                     isModerator BIT,
+                                     companyId INT,
+                                     FOREIGN KEY (companyId) REFERENCES Company(companyId)
 );
 
 INSERT INTO users (name, surname, username, email, birthdate, gender, password, isAdmin, isModerator)
 VALUES ('Admin', 'Admin', 'AdminUser', 'admin@example.com', '1980-05-15', 'Male', 'admin', 1, 1);
 
+
 CREATE TABLE IF NOT EXISTS Company(
-	companyId INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    userId INT,
-    FOREIGN KEY (userId) REFERENCES users(userId)
+                                      companyId INT AUTO_INCREMENT PRIMARY KEY,
+                                      name VARCHAR(100)
 );
+
 
 CREATE TABLE IF NOT EXISTS Product(
-	productId INT AUTO_INCREMENT PRIMARY KEY,
-    label VARCHAR(100),
-    price INT,
-	stock INT,
-    description VARCHAR(100),
-    companyId INT,
-	FOREIGN KEY (companyId) REFERENCES Company(companyId)
+                                      productId INT AUTO_INCREMENT PRIMARY KEY,
+                                      label VARCHAR(100),
+                                      price INT,
+                                      stock INT,
+                                      description VARCHAR(100),
+                                      companyId INT,
+                                      FOREIGN KEY (companyId) REFERENCES Company(companyId)
 );
 
+
 CREATE TABLE IF NOT EXISTS ProductOrder(
-	orderId INT AUTO_INCREMENT PRIMARY KEY,
-	orderDate DATE,
-	orderPrice INT,
-    userId INT,
-    FOREIGN KEY (userId) REFERENCES users(userId)
+                                           orderId INT AUTO_INCREMENT PRIMARY KEY,
+                                           orderDate DATE,
+                                           orderPrice INT,
+                                           userId INT,
+                                           FOREIGN KEY (userId) REFERENCES users(userId)
 );
