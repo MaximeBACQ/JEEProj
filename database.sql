1b8ff8f3305eac1a13c6a8bf44c64a5882844923
Lucaslas28 <lucasrigot@gmail.com>
2023-11-12 11:39:33 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,20 @@
 CREATE DATABASE IF NOT EXISTS database_JEE;
 
 USE database_JEE;
-DROP TABLE USERS;
+DROP TABLE IF EXISTS ProductOrder;
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS ProductCategory;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS Company;
+
+CREATE TABLE IF NOT EXISTS Company(
+                                      companyId INT AUTO_INCREMENT PRIMARY KEY,
+                                      name VARCHAR(100)
+);
+
+INSERT INTO Company (name)
+VALUES ('Cramptoux');
+
 CREATE TABLE IF NOT EXISTS users (
                                      userId INT AUTO_INCREMENT PRIMARY KEY,
                                      name VARCHAR(50),
@@ -18,23 +31,22 @@ CREATE TABLE IF NOT EXISTS users (
                                      FOREIGN KEY (companyId) REFERENCES Company(companyId)
 );
 UPDATE Users SET companyId=null WHERE userId=5;
-INSERT INTO users (name, surname, username, email, birthdate, gender, password, isAdmin, isModerator)
-VALUES ('Admin', 'Admin', 'AdminUser', 'admin@example.com', '1980-05-15', 'Male', 'admin', 1, 1);
-INSERT INTO users (name, surname, username, email, birthdate, gender, password, isAdmin, isModerator)
-VALUES ('Moderator', 'Moderator', 'ModUser', 'mod@example.com', '1980-05-15', 'Male', 'moderator', 0, 1);
-INSERT INTO users (name, surname, username, email, birthdate, gender, password, isAdmin, isModerator)
-VALUES ('User', 'User', 'User', 'user@example.com', '1980-05-15', 'Male', 'user', 0, 0);
+INSERT INTO users (name, surname, username, email, birthdate, gender, password, isAdmin, isModerator, isSeller)
+VALUES ('Admin', 'Admin', 'AdminUser', 'admin@example.com', '1980-05-15', 'Male', 'admin', 1, 1, 0);
+INSERT INTO users (name, surname, username, email, birthdate, gender, password, isAdmin, isModerator, isSeller)
+VALUES ('Moderator', 'Moderator', 'ModUser', 'mod@example.com', '1980-05-15', 'Male', 'moderator', 0, 1, 0);
+INSERT INTO users (name, surname, username, email, birthdate, gender, password, isAdmin, isModerator, isSeller)
+VALUES ('User', 'User', 'User', 'user@example.com', '1980-05-15', 'Male', 'user', 0, 0, 0);
 
-DROP TABLE Company;
-CREATE TABLE IF NOT EXISTS Company(
-                                      companyId INT AUTO_INCREMENT PRIMARY KEY,
-                                      name VARCHAR(100)
+CREATE TABLE IF NOT EXISTS ProductCategory(
+									categoryId INT AUTO_INCREMENT PRIMARY KEY,
+                                    categoryName VARCHAR(100)
 );
+INSERT INTO ProductCategory VALUES(1,"Guitares");
+INSERT INTO ProductCategory VALUES(2,"Basses");
+INSERT INTO ProductCategory VALUES(3,"Piano");
+INSERT INTO ProductCategory VALUES(4,"Batterie");
 
-INSERT INTO Company (name)
-VALUES ('Cramptoux');
-
-DROP TABLE Product;
 CREATE TABLE IF NOT EXISTS Product(
                                       productId INT AUTO_INCREMENT PRIMARY KEY,
                                       label VARCHAR(100),
@@ -42,10 +54,17 @@ CREATE TABLE IF NOT EXISTS Product(
                                       stock INT,
                                       description VARCHAR(100),
                                       companyId INT,
-                                      FOREIGN KEY (companyId) REFERENCES Company(companyId)
+                                      FOREIGN KEY (companyId) REFERENCES Company(companyId),
+                                      categoryId INT,
+                                      FOREIGN KEY (categoryId) REFERENCES ProductCategory(categoryId)
+                                      
 );
+INSERT INTO Product VALUES (1,"Ibanez",400,5,"Super guitare Ibanez",1,1);
+INSERT INTO Product VALUES (2,"Yamaha",500,7,"Incroyable guitare de pro",1,1);
+INSERT INTO Product VALUES (3,"Fender",250,4,"Super basse Fender",1,2);
+INSERT INTO Product VALUES (4,"Bechstein",400,5,"Super piano",1,3);
+INSERT INTO Product VALUES (5,"Gretsch",1200,2,"Super batterie",1,4);
 
-DROP TABLE ProductOrder;
 CREATE TABLE IF NOT EXISTS ProductOrder(
                                            orderId INT AUTO_INCREMENT PRIMARY KEY,
                                            orderDate DATE,
